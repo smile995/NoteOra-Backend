25e814134d23adf2f6f27a2fcbeba94658df63d5
smile995 <ah.smile995@gmail.com>
2025-08-27 13:26:00 +0600

    Use slugify to generate unique use friendly slug

--- src/app/modules/Post/post.service.ts
+++ src/app/modules/Post/post.service.ts
@@ -1,27 +1,32 @@
 import { TPost } from "./post.interface";
 import { PostModel } from "./post.model";
-
+import slugify from "slugify";
 const cteatePostIntoDB = async (post: TPost) => {
+  const slugWIthUnderscore = slugify(post.title, "_");
+  post.slug = slugWIthUnderscore;
   const result = await PostModel.create(post);
   return result;
 };
 const getAllPostFromDB = async () => {
-  const result = await PostModel.find({isDeleted:false});
+  const result = await PostModel.find({ isDeleted: false });
   return result;
 };
-const getSinglePostFromDB = async (id:string) => {
+const getSinglePostFromDB = async (id: string) => {
   const result = await PostModel.findById(id);
   return result;
 };
-const softDeleteSinglePostFromDB = async (id:string) => {
-  const result = await PostModel.findOneAndUpdate({_id:id}, {isDeleted:true},{new:true});
+const softDeleteSinglePostFromDB = async (id: string) => {
+  const result = await PostModel.findOneAndUpdate(
+    { _id: id },
+    { isDeleted: true },
+    { new: true }
+  );
   return result;
 };
 
-
-export  const PostServices={
-     cteatePostIntoDB,
-     getAllPostFromDB,
-     getSinglePostFromDB,
-     softDeleteSinglePostFromDB
-}
+export const PostServices = {
+  cteatePostIntoDB,
+  getAllPostFromDB,
+  getSinglePostFromDB,
+  softDeleteSinglePostFromDB,
+};
